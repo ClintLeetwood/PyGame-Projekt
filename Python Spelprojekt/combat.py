69060e4aea91b51c0f9913e44e2dc55049501587
Simon Strindberg <simon.strindberg@hotmail.se>
2022-11-01 11:03:43 +0100

    awdawdaf

---
+++ Python Spelprojekt/combat.py
@@ -0,0 +1,24 @@
+from re import A
+from this import d
+from typing_extensions import Self
+from xmlrpc.client import Boolean
+from Python Spelprojekt.sprites import Player
+import pygame
+import sprites
+
+class Fight:
+    def __init__(self,menu,x,y):
+    
+    def fight(self,win):
+        while Player()
+        win=0
+        cpuchoice=0
+        self.playerattack=
+        self.playerdefend=False         
+        self.enemydefend=False 
+        self.enemyattack=
+        
+        
+        self.exec
+
+        
